e78ab39778e21ff0cf8097e9b4b483ad08346bf2
Sithira Buddhika <sithirabuddhika@gmail.com>
2025-07-22 17:33:19 +0530

    update apis

--- src/services/api.js
+++ src/services/api.js
@@ -4,3 +4,26 @@ const API_BASE_URL = "http://localhost:8005/api";
 export const api = axios.create({
   baseURL: API_BASE_URL,
 });
+
+//Auth APIs
+export const login = (credentials) => {
+  return api.post("/users/login", credentials);
+};
+
+//Exam APIs
+export const getAllExams = () => {
+  api.get("/exams");
+};
+
+export const getQuestionsByExam = (examId) => {
+  api.get(`/questions/exams/${examId}`);
+};
+
+//Result APIs
+export const submitResult = (resultData) => {
+  api.post("/results", resultData);
+};
+
+export const getResultsById = (resultId) => {
+  api.get(`/results/${resultId}`);
+};
